9a0e7a9bd35729e4f6c5c0926061895a32a9c099
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2016-01-21 13:28:19 +0100

    add some init methods

---
+++ src/main/java/de/citec/csra/init/Initializer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.citec.csra.init;
+
+/**
+ *
+ * @author Patrick Holthaus
+ * (<a href=mailto:patrick.holthaus@uni-bielefeld.de>patrick.holthaus@uni-bielefeld.de</a>)
+ */
+public interface Initializer<K, V> {
+	
+	public abstract V initialize(K key);
+	
+}
