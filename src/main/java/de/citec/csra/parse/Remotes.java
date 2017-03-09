69dcb1e4b4e0630aa3dde6fd7d53017579076570
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2017-03-09 13:28:50 +0100

    use newer ha interface

--- src/main/java/de/citec/csra/parse/Remotes.java
+++ src/main/java/de/citec/csra/parse/Remotes.java
@@ -7,15 +7,17 @@ package de.citec.csra.parse;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.openbase.bco.dal.remote.unit.ColorableLightRemote;
 import org.openbase.bco.dal.remote.unit.DimmerRemote;
 import org.openbase.bco.registry.location.remote.LocationRegistryRemote;
+import org.openbase.bco.registry.remote.Registries;
 import org.openbase.bco.registry.unit.remote.UnitRegistryRemote;
 import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.jul.exception.InitializationException;
-import rst.domotic.unit.UnitConfigType;
+import rst.domotic.unit.UnitConfigType.UnitConfig;
 
 /**
  *
@@ -24,71 +26,67 @@ import rst.domotic.unit.UnitConfigType;
  */
 public class Remotes {
 
-    private final Map<UnitConfigType.UnitConfig, ColorableLightRemote> lights = new HashMap<>();
-    private final Map<UnitConfigType.UnitConfig, DimmerRemote> dimmers = new HashMap<>();
-    private LocationRegistryRemote locations;
-    private UnitRegistryRemote unitRegistry;
-    private final static Logger LOG = Logger.getLogger(Remotes.class.getName());
+	private final Map<UnitConfig, ColorableLightRemote> lights = new HashMap<>();
+	private final Map<UnitConfig, DimmerRemote> dimmers = new HashMap<>();
+	private LocationRegistryRemote locations;
+	private UnitRegistryRemote unitRegistry;
+	private final static Logger LOG = Logger.getLogger(Remotes.class.getName());
 
-    private static Remotes instance;
+	private static Remotes instance;
 
-    private Remotes() {
-    }
+	private Remotes() {
+	}
 
-    public static Remotes get() {
-        if (instance == null) {
-            instance = new Remotes();
-        }
-        return instance;
-    }
+	public static Remotes get() {
+		if (instance == null) {
+			instance = new Remotes();
+		}
+		return instance;
+	}
 
-    public ColorableLightRemote getColorableLight(UnitConfigType.UnitConfig u) throws InitializationException, InterruptedException, CouldNotPerformException {
-        if (this.lights.containsKey(u)) {
-            return this.lights.get(u);
-        } else {
-            LOG.log(Level.FINE, "initializing ambient light remote for unit ''{0}''", u.getLabel());
-            ColorableLightRemote remote = new ColorableLightRemote();
-            remote.init(u);
-            remote.activate();
-            remote.waitForData();
-            this.lights.put(u, remote);
-            return remote;
-        }
-    }
+	public ColorableLightRemote getColorableLight(UnitConfig u, long timeout) throws InitializationException, InterruptedException, CouldNotPerformException {
+		if (this.lights.containsKey(u)) {
+			return this.lights.get(u);
+		} else {
+			LOG.log(Level.FINE, "initializing ambient light remote for unit ''{0}''", u.getLabel());
+			ColorableLightRemote remote = new ColorableLightRemote();
+			remote.init(u);
+			remote.activate(false);
+			remote.waitForData(timeout, TimeUnit.MILLISECONDS);
+			this.lights.put(u, remote);
+			return remote;
+		}
+	}
 
-    public DimmerRemote getDimmableLight(UnitConfigType.UnitConfig u) throws InitializationException, InterruptedException, CouldNotPerformException {
-        if (this.dimmers.containsKey(u)) {
-            return this.dimmers.get(u);
-        } else {
-            LOG.log(Level.FINE, "initializing dimmer remote for unit ''{0}''", u.getLabel());
-            DimmerRemote remote = new DimmerRemote();
-            remote.init(u);
-            remote.activate();
-            remote.waitForData();
-            this.dimmers.put(u, remote);
-            return remote;
-        }
-    }
+	public DimmerRemote getDimmableLight(UnitConfig u, long timeout) throws InitializationException, InterruptedException, CouldNotPerformException {
+		if (this.dimmers.containsKey(u)) {
+			return this.dimmers.get(u);
+		} else {
+			LOG.log(Level.FINE, "initializing dimmer remote for unit ''{0}''", u.getLabel());
+			DimmerRemote remote = new DimmerRemote();
+			remote.init(u);
+			remote.activate(false);
+			remote.waitForData(timeout, TimeUnit.MILLISECONDS);
+			this.dimmers.put(u, remote);
+			return remote;
+		}
+	}
 
-    public LocationRegistryRemote getLocationRegistry() throws InstantiationException, InitializationException, InterruptedException, CouldNotPerformException {
-        if (this.locations == null) {
-            LOG.log(Level.FINE, "initializing location registry remote");
-            this.locations = new LocationRegistryRemote();
-            this.locations.init();
-            this.locations.activate();
-            this.locations.waitForData();
-        }
-        return this.locations;
-    }
+	public LocationRegistryRemote getLocationRegistry(long timeout) throws InstantiationException, InitializationException, InterruptedException, CouldNotPerformException {
+		if (this.locations == null) {
+			LOG.log(Level.FINE, "initializing location registry remote");
+			this.locations = Registries.getLocationRegistry();
+			this.locations.waitForData(timeout, TimeUnit.MILLISECONDS);
+		}
+		return this.locations;
+	}
 
-    public UnitRegistryRemote getUnitRegistry() throws InstantiationException, InitializationException, InterruptedException, CouldNotPerformException {
-        if (this.unitRegistry == null) {
-            LOG.log(Level.FINE, "initializing device registry remote");
-            this.unitRegistry = new UnitRegistryRemote();
-            this.unitRegistry.init();
-            this.unitRegistry.activate();
-            this.unitRegistry.waitForData();
-        }
-        return this.unitRegistry;
-    }
+	public UnitRegistryRemote getUnitRegistry(long timeout) throws InstantiationException, InitializationException, InterruptedException, CouldNotPerformException {
+		if (this.unitRegistry == null) {
+			LOG.log(Level.FINE, "initializing device registry remote");
+			this.unitRegistry = Registries.getUnitRegistry();
+			this.unitRegistry.waitForData(timeout, TimeUnit.MILLISECONDS);
+		}
+		return this.unitRegistry;
+	}
 }
